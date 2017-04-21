def9e09436673dc376aa005e51966697f545a697
jotish <jotish@ymail.com>
2017-04-21 20:44:54 +0530

    fixing things

--- app/src/main/java/com/indira/usedbooks/GetUserInterface.java
+++ app/src/main/java/com/indira/usedbooks/GetUserInterface.java
@@ -10,6 +10,8 @@ import com.indira.usedbooks.entity.User;
 //import okhttp3.RequestBody;
 import retrofit2.Call;
 //import retrofit2.http.GET;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
@@ -25,11 +27,20 @@ public interface GetUserInterface {
 
 
     //@GET("login.php")
-   // Call<User> getUser(@Path("email") String email, @Path("password") String password);
+   // Call<User> getUser(@Field("email") String email, @Field("password") String password);
 
 
     @POST("register.php")
-    Call<Response> addUser(@Path("name") String name, @Path("email") String email, @Path("password") String password, @Path("phoneno") String phoneno, @Path("phoneno2") String phoneno2, @Path("address") String address, @Path("city") String city, @Path("state") String state);
+    @FormUrlEncoded
+    Call<Response> addUser(
+        @Field("name") String name,
+        @Field("email") String email,
+        @Field("password") String password,
+        @Field("phoneno") String phoneno,
+        @Field("phoneno2") String phoneno2,
+        @Field("address") String address,
+        @Field("city") String city,
+        @Field("state") String state);
 }
 
 
