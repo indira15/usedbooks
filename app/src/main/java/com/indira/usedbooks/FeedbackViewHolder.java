16acdf3721e90669fe7e580cea14d7d83ac34338
Manish Jangid <Manish Jangid>
2017-05-13 22:09:26 +0530

    feedback init stage

--- app/src/main/java/com/indira/usedbooks/FeedbackViewHolder.java
+++ app/src/main/java/com/indira/usedbooks/FeedbackViewHolder.java
@@ -1,8 +1,14 @@
 package com.indira.usedbooks;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
 /**
  * Created by Manish on 09-05-2017.
  */
 
-public class FeedbackViewHolder {
+public class FeedbackViewHolder extends RecyclerView.ViewHolder {
+    public FeedbackViewHolder(View itemView) {
+        super(itemView);
+    }
 }
